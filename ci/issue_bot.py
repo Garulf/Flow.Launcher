a8349ef295fd45e919e90435c00de1c383e7abae
Garulf <535299+Garulf@users.noreply.github.com>
2023-04-25 09:22:40 -0400

    Update issue_bot.py

--- ci/issue_bot.py
+++ ci/issue_bot.py
@@ -1,6 +1,8 @@
 import sys
 import re
-from typing import Optional, Tuple
+import os
+import uuid
+from typing import List, Optional, Tuple
 
 EMPTY_LOG = r"\`\`\`shell.*Replace this line with the important log contents\..*\`\`\`.*</details>.*<!-- # Or drag and drop the log file and delete the above detail part. -->.*"
 
@@ -17,10 +19,22 @@ CHECKS = [
 ]
 
 
-def output_problem(problem: Optional[Tuple[str, str]]) -> None:
+def set_multiline_output(output: List[str]):
+    if 'GITHUB_OUTPUT' not in os.environ:
+        filename = f"{uuid.uuid4()}.md"
+        os.environ['GITHUB_OUTPUT'] = filename
+        print(f"::set-output name=filename::{filename}")
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        for line in output:
+            print(line, file=fh)
+
+
+def output_problem(output: List[str], problem: Optional[Tuple[str, str]]) -> None:
     if problem:
-        print(f"## {problem[0]}\n")
-        print(f"{problem[1]}\n")
+        output.append(f"## {problem[0]}")
+        output.append("")
+        output.append(f"{problem[1]}")
+        output.append("")
 
 
 def main():
@@ -33,11 +47,15 @@ def main():
             problems.append(result)
 
     if problems:
-        print("# The following problems were found:\n")
+        output = []
+        output.append("# The following problems were found:")
+        output.append("")
         for problem in problems:
-            output_problem(problem)
+            output_problem(output, problem)
 
-        print("\nPlease consider these recommendations so we may help you better.")
+        output.append("\nPlease consider these recommendations so we may help you better.")
+
+        set_multiline_output(output)
 
 
 if __name__ == "__main__":
