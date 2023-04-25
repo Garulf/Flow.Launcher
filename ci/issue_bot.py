f841fc8a780bfa79639fb585bf2739895a5bf0b2
Garulf <535299+Garulf@users.noreply.github.com>
2023-04-25 08:24:38 -0400

    Update issue_bot.py

--- ci/issue_bot.py
+++ ci/issue_bot.py
@@ -1,1 +1,34 @@
-print("Hellow World!!")
+import sys
+
+EMPTY_LOG = """```shell
+
+Replace this line with the important log contents.
+
+```
+</details>
+
+<!-- # Or drag and drop the log file and delete the above detail part. -->"""
+
+
+def missing_log(body: str):
+    if EMPTY_LOG in body:
+        return "In order to help us debug your issue please provide a log file. Thank you!"
+
+
+CHECKS = [
+    missing_log
+]
+
+
+def main():
+    issue_body = sys.argv[1]
+    problems = []
+    for check in CHECKS:
+        result = check(issue_body)
+        if result:
+            problems.append(result)
+
+    if problems:
+        print("The following problems were found:")
+        for problem in problems:
+            print(f"* {problem}")
